8686799078a464e0aeafb4c2e388f9522ec09920
Jyothi-Vummadi <90172590+Jyothi-Vummadi@users.noreply.github.com>
2022-01-31 22:57:55 -0800

    Graphs : DFS

---
+++ dfs.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int a[20][20],reach[20],n;
+void dfs(int v) {
+	int i;
+	reach[v]=1;
+	for (i=1;i<=n;i++)
+	  if(a[v][i] && !reach[i]) {
+		printf("%d->%d\n",v,i);
+		dfs(i);
+	}
+}
+void main() {
+	int i,j,count=0;
+		printf("Enter number of vertices:");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++) {
+		reach[i]=0;
+		for (j=1;j<=n;j++)
+		   a[i][j]=0;
+	}
+	printf("Enter the adjacency matrix:\n");
+	for (i=1;i<=n;i++)
+	  for (j=1;j<=n;j++)
+	   scanf("%d",&a[i][j]);
+	printf("DFS Traversal: \n");
+	dfs(1);
+	}
